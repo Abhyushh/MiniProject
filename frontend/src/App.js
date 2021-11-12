3e6d44903280432a56d395b1a1561628b0b76cea
Abhyushh <aanand3_be19@thapar.edu>
2021-11-12 17:32:54 +0530

    Frontend Commit

---
+++ frontend/src/App.js
@@ -0,0 +1,15 @@
+import React, { Component } from "react";
+
+import SideBar from "./Component/Navbar/Sidebar";
+import "./App.css";
+
+function App() {
+  return (
+      <React.Fragment>
+        <div className="App">
+          <SideBar />
+        </div>
+      </React.Fragment>
+    );
+}
+export default App;
