cafb1c9c693027ddea6411740fda30e6a5038bfd
aki09 <akshatbhatia05@gmail.com>
2021-12-21 23:30:46 +0530

    remove

--- backend/app.js
+++ backend/app.js
@@ -1,15 +1,8 @@
 const express = require("express");
-const mongoose = require("mongoose");
-const session = require('express-session');
-const mongodbStore = require('connect-mongodb-session')(session);
 const bodyParser = require("body-parser");
 const path = require('path');
 const cors = require('cors');
 const cookieParser = require('cookie-parser');
-var http = require('http');
-var request = require('request');
-var querystring = require('querystring');
-var exec = require('child_process').exec;
 
 require('dotenv').config();
 
@@ -20,10 +13,6 @@ const userRoutes = require('./routes/user');
 const { post } = require("request");
 
 const app = express();
-const store = new mongodbStore({
-    uri: dbUri,
-    collection: 'sessions'
-});
 const PORT = process.env.PORT || 5000;
 
 const spotifyApi = require('./spotifyApi').init();
@@ -33,13 +22,6 @@ app.use(bodyParser.json());
 app.use(express.static(path.join(__dirname, '/public')));
 app.use(cors());
 app.use(cookieParser());
-app.use(
-    session({
-        secret: 'secret key',
-        resave: false,
-        saveUninitialized: false,
-        store: store
-    }));
 
 app.use(authRoutes);
 app.use(userRoutes);
